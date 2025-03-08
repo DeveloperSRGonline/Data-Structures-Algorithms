6d8dad780b5cfc0ca44dfb58e9aed8c26c336a1e
Shivam Garade <shivamgarade05@gmail.com>
2025-03-08 13:05:22 +0530

    .

---
+++ 27-DSA basics/script.js
@@ -0,0 +1,43 @@
+// -- In the dsa you should use "let"
+// -- console.log pe kuchh bhi "black" aaya to vo sring hota hai  
+// let a = 10;
+// console.log(a); // it will print 10
+
+// integer yane aisa number jisme koi decimal na ho 
+
+// Q.sum of two integer:
+
+    let a = 20;
+    let b = 20;
+    console.log(a+b);
+
+// Q.Relation between integer and string:
+
+/*
+string + string = string (concatenation)
+    let a = "hello";
+    let b = "world";
+
+    console.log(a + b); // Output: helloworld
+    console.log(typeof(a + b)); // Output: string
+
+string + int = string (concatenation)
+    let a = "hello";
+    let b = 12;
+
+    console.log(a + b); // Output: hello12
+    console.log(typeof(a + b)); // Output: string
+
+int + int = int (addition)
+    let a = 12;
+    let b = 12;
+
+    console.log(a + b); // Output: 24
+    console.log(typeof(a + b)); // Output: number
+
+    let a = 10
+    let b = 20
+
+    console.log("sum of " + a + " and " + b + " is " + a + b)
+*/
+
