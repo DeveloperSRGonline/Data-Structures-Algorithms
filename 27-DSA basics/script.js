8c8b9a1a6a5038e1889bd4c9718ca525191c4abc
Shivam Garade <shivamgarade05@gmail.com>
2025-03-09 02:23:01 +0530

    .

--- 27-DSA basics/script.js
+++ 27-DSA basics/script.js
@@ -1,15 +1,16 @@
-// -- In the dsa you should use "let"
+// -- In the DSA you should use "let"
 // -- console.log pe kuchh bhi "black" aaya to vo sring hota hai  
 // let a = 10;
 // console.log(a); // it will print 10
 
-// integer yane aisa number jisme koi decimal na ho 
+// integer yane aisa number jisme koi decimal na ho eg.23 , 3443, 345545
 
 // Q.sum of two integer:
 
-    let a = 20;
-    let b = 20;
-    console.log(a+b);
+    let a = 12;
+    let b = 23;
+
+    console.log(a+b) // it will print 35
 
 // Q.Relation between integer and string:
 
@@ -17,27 +18,64 @@
 string + string = string (concatenation)
     let a = "hello";
     let b = "world";
-
     console.log(a + b); // Output: helloworld
-    console.log(typeof(a + b)); // Output: string
+    console.log(typeof(a + b)); // Output: string 
+
+    let a = "hello"
+    let b = "shivam"
+    console.log(a + b) // it will print : helloshivam
+    console.log(typeof(a + b)) // it will print : string
 
 string + int = string (concatenation)
     let a = "hello";
     let b = 12;
-
     console.log(a + b); // Output: hello12
     console.log(typeof(a + b)); // Output: string
+
+    let a = "hello"
+    let b = 23;
+    console.log(a + b); // it will print : hello23
+    console.log(typeof(a + b)) // it will print : string
 
 int + int = int (addition)
     let a = 12;
     let b = 12;
-
     console.log(a + b); // Output: 24
     console.log(typeof(a + b)); // Output: number
 
+
     let a = 10
     let b = 20
+    console.log(a + b); // it will print : number
+    console.log("sum of " + a + " and " + b + " is " + a + b)❌
+                "sum of 10" + " and " + b + " is " + a + b
+                "sum of 10 and " + b + " is " + a + b
+                "sum of 10 and 20" + " is " + a + b
+                "sum of 10 and 20 is " + a + b                   
+                "sum of 10 and 20 is 10 + b                   
+                "sum of 10 and 20 is 1020     
+    console.log("sum of " + a + " and " + b + " is " + (a + b))✅             
+    console.log( (a + b) + " is " + "sum of " + a + " and " + b) // it will print : 30 is sum of 10 and 20    
+    
+    console.log(1 + 1) --> agar operator ke dono side int hai to ye arithmatic operation karega 
+    and 
+    if 
+    console.log(1 + "1") --> agar operator ke koi ek bhi side string hai toh ye concatinate karega
+    
 
-    console.log("sum of " + a + " and " + b + " is " + a + b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 */
 
