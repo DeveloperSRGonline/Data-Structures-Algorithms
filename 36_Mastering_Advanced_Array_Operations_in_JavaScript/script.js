b19e5d084aef6c91e34913435b140874308265dc
Shivam Garade <shivamgarade05@gmail.com>
2025-03-22 19:45:20 +0530

    calculations for second minimum and minimum values in an array

--- 36_Mastering_Advanced_Array_Operations_in_JavaScript/script.js
+++ 36_Mastering_Advanced_Array_Operations_in_JavaScript/script.js
@@ -5,7 +5,7 @@
 // -- secondMax = Math.min(arr[0], arr[1]); inme se jo chota aayega vo secondMax
 // -- ab ham array ke 2nd element se loop chalayenge aur check karenge ki vo max se bada hai ya nahi agar bada hai to usko max me store karenge aur max ko secondMax me store karenge
 // -- agar vo max se chota hai to usko secondMax me store karenge
-
+/*
 let arr = [28,6,74,48,84,79];
 let max = Math.max(arr[0], arr[1]); 
 let secondMax = Math.min(arr[0], arr[1]);
@@ -19,3 +19,60 @@ for(let i = 2; i < arr.length; i++){
     }
 }
 console.log("second max : " + secondMax);  
+
+
+// Q.2.Calculate second-min element in an array:
+let min = Math.min(arr[0], arr[1]);
+let secondMin = Math.max(arr[0], arr[1]);
+
+for (let i = 2; i < arr.length; i++) {
+    if (arr[i] < min) {
+        secondMin = min;
+        min = arr[i];
+    } else if (arr[i] < secondMin) {
+        secondMin = arr[i];
+    }
+}
+console.log("Second minimum value: " + secondMin);
+
+
+// Q.Finding the Minimum Value in an Array:
+let arr = [28, 6, 74, 48, 84, 79];
+let min = arr[0]; 
+
+for (let i = 1; i < arr.length; i++) { 
+    if (arr[i] < min) { 
+        min = arr[i]; 
+    } 
+} 
+
+console.log("Minimum value: " + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
