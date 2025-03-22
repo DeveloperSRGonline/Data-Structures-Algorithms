28c590aef72830ce0172d926eb7dea9677943602
Shivam Garade <shivamgarade05@gmail.com>
2025-03-22 10:38:19 +0530

    Fix spacing issue in second maximum element calculation

--- 36_Mastering_Advanced_Array_Operations_in_JavaScript/script.js
+++ 36_Mastering_Advanced_Array_Operations_in_JavaScript/script.js
@@ -13,7 +13,7 @@ let secondMax = Math.min(arr[0], arr[1]);
 for(let i = 2; i < arr.length; i++){
     if(arr[i] > max){
         secondMax = max;
-        max = arr[i];
+        max = arr[i]; 
     }else if(arr[i] > secondMax){
         secondMax = arr[i];
     }
